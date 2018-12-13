b0e8ae6e8315d2b03e7e78f581a224a54c1ad3a9
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2018-12-13 11:18:35 +0100

    fungerar

--- src/integraltest.java
+++ src/integraltest.java
@@ -4,10 +4,14 @@ public class integraltest {
     public static void main(String[] args) {
         Scanner tgb= new Scanner(System.in);
             double a,b;
+
             System.out.println("Undre gräns?");
+
             a = tgb.nextDouble();
+
             System.out.println("Övre gräns?");
             b = tgb.nextDouble();
-        System.out.println(integraler.integral(a , b));
+
+            System.out.println(integraler.integral(a , b));
     }
 }
